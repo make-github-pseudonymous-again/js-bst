734dddceb81b06ff4fe4288dad482075d567f689
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:45 +0100

    aureooms- > @aureooms/ : update js code

--- test/src/all.js
+++ test/src/all.js
@@ -1,9 +1,9 @@
 import test from 'ava' ;
 
-import compare from "aureooms-js-compare" ;
-import random from "aureooms-js-random" ;
-import * as it from "aureooms-js-itertools" ;
-import fn from "aureooms-js-functools" ;
+import compare from "@aureooms/js-compare" ;
+import random from "@aureooms/js-random" ;
+import * as it from "@aureooms/js-itertools" ;
+import fn from "@aureooms/js-functools" ;
 
 import bst from '../../src' ;
 
